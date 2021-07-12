901c159fd1e5acfca98b47252a12c64bd3520582
Lucas Ference <ference.lucas@gmail.com>
2021-07-11 22:41:10 -0400

    Fix gallery size rendering

--- src/views/gallery/gallery.js
+++ src/views/gallery/gallery.js
@@ -2,26 +2,19 @@ import React from 'react';
 import './gallery.css';
 
 import ImageGallery from 'react-image-gallery';
-import useWindowDimensions from 'util/window';
 
-function getGalleryImages(height, width, ...values) {
-    const multiplier = width > 768 ? 0.7 : 0.5; 
-    const imageHeight = height * multiplier;
-
+function getGalleryImages(...values) {
     return values.map(v => {
         return {
             original: process.env.PUBLIC_URL + '/img/gallery/' + v,
-            originalHeight: imageHeight
+            originalClass: 'gallery-image'
         }
     });
 }
 
 const Gallery = () => {
 
-    const { height, width } = useWindowDimensions();
-
     const images = getGalleryImages(
-        height, width,
         'gi1.jpg', 'gi2.jpg', 'gi3.jpeg', 'gi4.jpg', 'gi10.jpg',
         'gi5.jpg', 'gi8.jpg', 'gi6.jpeg', 'gi7.jpg', 'gi9.jpg'
     );
@@ -34,7 +27,6 @@ const Gallery = () => {
                 additionalClass="gallery-container"
                 items={images}
                 lazyLoad={true}
-                showFullscreenButton={false}
                 showThumbnails={false}
             />
         </div>
