0e45f185bd4c56916c05b35b76f1fcf15c42c527
Lucas Ference <ference.lucas@gmail.com>
2022-02-20 15:54:36 -0500

    Update info section

--- src/views/info/info.js
+++ src/views/info/info.js
@@ -22,17 +22,7 @@ class Info extends React.Component {
                 <h3 className="info-subhead">Dress Code</h3>
                 <div className="info-detail">
                     <p>
-                        The dress code is semi-formal. Dress shirt and dress pants is fine.
-                        No need for a suit unless you want!
-                    </p>
-                </div>
-
-                <hr />
-
-                <h3 className="info-subhead">Lodging Info</h3>
-                <div className="info-detail">
-                    <p>
-                        The last day to book a hotel room through the provided links is May 13th, 2022.
+                        The dress code is business-casual.
                     </p>
                 </div>
             </div>
